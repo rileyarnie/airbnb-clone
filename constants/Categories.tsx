3e128ad94c7a8cb2806728fedbb7ab003c9d94a1
Arnold Kibira <arnold.ril3y@gmail.com>
2023-11-19 22:48:22 +0300

    add dummy categories to be displayed

---
+++ constants/Categories.tsx
@@ -0,0 +1,32 @@
+const Categories = [
+  {
+    name: "Tiny homes",
+    icon: "home",
+  },
+  {
+    name: "Cabins",
+    icon: "house-siding",
+  },
+  {
+    name: "Trending",
+    icon: "local-fire-department",
+  },
+  {
+    name: "Play",
+    icon: "videogame-asset",
+  },
+  {
+    name: "City",
+    icon: "apartment",
+  },
+  {
+    name: "Beachfront",
+    icon: "beach-access",
+  },
+  {
+    name: "Countryside",
+    icon: "nature-people",
+  },
+];
+
+export default Categories;
